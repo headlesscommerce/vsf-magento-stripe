3b0b3079a22e9a67342d3ba58adf63198759a290
Beniamin Sinca <beni.sinca@gmail.com>
2022-12-19 10:58:34 +0000

    fix(imports/exports): remove types

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import Stripe from "./stripe/components/payment.vue";
-import { PaymentIntentClientSecret, PaymentIntentOptions, StripePaymentsToken, StripeEvents } from '~/stripe/types/types';
 
 export {
-    Stripe,
-    PaymentIntentClientSecret,
-    PaymentIntentOptions,
-    StripePaymentsToken,
-    StripeEvents
+    Stripe
 }
